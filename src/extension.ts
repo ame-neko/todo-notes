e5f353eba6f5686932b856693fdb42178522f224
miyamoto <59051990+pixel-png@users.noreply.github.com>
2022-10-08 11:53:54 +0900

    add treeView of tags

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,79 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import {completeTodo} from './completeTodo';
+import { completeTodo } from "./completeTodo";
+import { NotesTagsProvider } from "./tagsTreeView";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "todo-notes" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "todo-notes" is now active!');
 
-	let addTodoDisposable = vscode.commands.registerCommand('todo-notes.addTodo', () => {
-		const TODO_MARKDOWN = "- [ ] "
-		const editor = vscode.window.activeTextEditor;
-		editor?.edit(e => {e.insert(new vscode.Position(editor.selection.active.line, 0), TODO_MARKDOWN)});
-	})
-	context.subscriptions.push(addTodoDisposable);
+  let addTodoDisposable = vscode.commands.registerCommand(
+    "todo-notes.addTodo",
+    () => {
+      const TODO_MARKDOWN = "- [ ] ";
+      const editor = vscode.window.activeTextEditor;
+      editor?.edit((e) => {
+        e.insert(
+          new vscode.Position(editor.selection.active.line, 0),
+          TODO_MARKDOWN
+        );
+      });
+    }
+  );
+  context.subscriptions.push(addTodoDisposable);
 
-	let completeTodoDisposable = vscode.commands.registerCommand('todo-notes.completeTodo', () => {
-		// const editor = vscode.window.activeTextEditor;
-		// if (editor){
-		// 	const currentLine = editor.document.lineAt(editor.selection.active.line)
-		// 	const newLine = currentLine.text.replace(/^- \[ \]/, "- [x]", )
-		// 	editor?.edit(e => {e.replace(currentLine.range, newLine)});
-		// }
-		completeTodo();
-	})
-	context.subscriptions.push(completeTodoDisposable);
+  let completeTodoDisposable = vscode.commands.registerCommand(
+    "todo-notes.completeTodo",
+    () => {
+      // const editor = vscode.window.activeTextEditor;
+      // if (editor){
+      // 	const currentLine = editor.document.lineAt(editor.selection.active.line)
+      // 	const newLine = currentLine.text.replace(/^- \[ \]/, "- [x]", )
+      // 	editor?.edit(e => {e.replace(currentLine.range, newLine)});
+      // }
+      completeTodo();
+    }
+  );
+  context.subscriptions.push(completeTodoDisposable);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('todo-notes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Todo Notes!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "todo-notes.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from Todo Notes!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+
+  const rootPath =
+    vscode.workspace.workspaceFolders &&
+    vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : undefined;
+  if (rootPath) {
+    let treeViewDisposable = vscode.window.createTreeView("todoNotesTags", {
+      treeDataProvider: new NotesTagsProvider(rootPath),
+    });
+    treeViewDisposable.onDidChangeSelection((e) => {
+      if (e.selection.length > 0 && e.selection[0].path) {
+        const openPath = vscode.Uri.file(e.selection[0].path);
+        vscode.workspace.openTextDocument(openPath).then((doc) => {
+          vscode.window.showTextDocument(doc);
+        });
+      }
+    });
+    context.subscriptions.push(treeViewDisposable);
+  }
 }
 
 // this method is called when your extension is deactivated
---
+++ src/tagsTreeView.ts
@@ -0,0 +1,109 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+const frontMatter = require("front-matter");
+
+class Element extends vscode.TreeItem {
+  type: "tag" | "file";
+  name: string;
+  path: string | null;
+  constructor(type: "tag" | "file", name: string, path: string | null) {
+    const collapsibleState: vscode.TreeItemCollapsibleState =
+      type === "tag"
+        ? vscode.TreeItemCollapsibleState.Collapsed
+        : vscode.TreeItemCollapsibleState.None;
+    super(name, collapsibleState);
+    this.type = type;
+    this.name = name;
+    this.path = path;
+  }
+}
+
+export class NotesTagsProvider implements vscode.TreeDataProvider<Element> {
+  tagToElements: { [key: string]: Element[] };
+  constructor(private workspaceRoot: string) {
+    this.tagToElements = {};
+  }
+
+  getTreeItem(element: Element): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: Element): Thenable<Element[]> {
+    if (!this.workspaceRoot) {
+      vscode.window.showInformationMessage("No dependency in empty workspace");
+      return Promise.resolve([]);
+    }
+
+    if (element) {
+      // children
+      if (element.name in this.tagToElements) {
+        return Promise.resolve(this.tagToElements[element.name]);
+      } else {
+        return Promise.resolve([]);
+      }
+    } else {
+      // root level
+      return this.getAllTags();
+    }
+  }
+
+  async walk(dirPath: string): Promise<Element[]> {
+    const filePaths: Element[] = [];
+    const dirents = await fs.promises.readdir(dirPath, { withFileTypes: true });
+    dirents
+      .filter((dirent) => !dirent.isDirectory())
+      .forEach((dirent) => {
+        const fp = path.join(dirPath, dirent.name);
+        filePaths.push(new Element("file", dirent.name, fp));
+      });
+    await Promise.all(
+      dirents
+        .filter((dirent) => dirent.isDirectory())
+        .map(async (dirent) => {
+          const fp = path.join(dirPath, dirent.name);
+          const children = await this.walk(fp);
+          filePaths.push(...children);
+        })
+    );
+    return filePaths;
+  }
+
+  async extractTagFromNote(fp: string): Promise<string[]> {
+    const data = await fs.promises.readFile(fp, "utf-8");
+    const yamlHeader = frontMatter(data);
+    if (yamlHeader?.attributes?.tags) {
+      if (Array.isArray(yamlHeader?.attributes?.tags)) {
+        return yamlHeader?.attributes?.tags;
+      } else if (typeof yamlHeader?.attributes?.tags === "string") {
+        return [yamlHeader?.attributes?.tags];
+      }
+    }
+    return [];
+  }
+
+  async getAllTags() {
+    const elements = await this.walk(this.workspaceRoot);
+    await Promise.all(
+      elements.map(async (element) => {
+        if (!element.path) {
+          return;
+        }
+        const tags = await this.extractTagFromNote(element.path);
+        tags.forEach((tag) => {
+          if (tag in this.tagToElements) {
+            this.tagToElements[tag].push(element);
+          } else {
+            this.tagToElements[tag] = [element];
+          }
+        });
+      })
+    );
+
+    return Promise.resolve(
+      Object.keys(this.tagToElements).map(
+        (key) => new Element("tag", key, null)
+      )
+    );
+  }
+}
