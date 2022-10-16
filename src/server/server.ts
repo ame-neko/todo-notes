ecc3050a83ee6c65eb317306ad07415786c1304e
miyamoto <59051990+pixel-png@users.noreply.github.com>
2022-10-17 00:30:32 +0900

    refactor

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,20 +7,12 @@ import {
   FILE_SAVED_NOTIFICATION_METHOD,
 } from "./../constants";
 import { TagHandler } from "./tagParser";
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 import {
   createConnection,
   TextDocuments,
-  Diagnostic,
-  DiagnosticSeverity,
   ProposedFeatures,
   InitializeParams,
-  DidChangeConfigurationNotification,
   CompletionItem,
-  CompletionItemKind,
   TextDocumentPositionParams,
   TextDocumentSyncKind,
   InitializeResult,
@@ -33,28 +25,16 @@ import { CreateVirtualDocumentParams, CREATE_VIRTUAL_DOCUMENT_METHOD, GetAllTags
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
-// console.log = connection.console.log.bind(connection.console);
-// console.error = connection.console.error.bind(connection.console);
-// Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
-let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
-let hasDiagnosticRelatedInformationCapability = false;
 
 connection.onInitialize((params: InitializeParams) => {
-  console.log("initializing language server");
   const capabilities = params.capabilities;
 
   // Does the client support the `workspace/configuration` request?
   // If not, we fall back using global settings.
-  hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
   hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
-  hasDiagnosticRelatedInformationCapability = !!(
-    capabilities.textDocument &&
-    capabilities.textDocument.publishDiagnostics &&
-    capabilities.textDocument.publishDiagnostics.relatedInformation
-  );
 
   const result: InitializeResult = {
     capabilities: {
@@ -102,36 +82,6 @@ connection.onNotification(FILE_SAVED_NOTIFICATION_METHOD, (params: FileSavedPara
     const params: RefreshTagsTreeParams = { tagsToElement: tagsToElement };
     connection.sendNotification(REFRESH_TAGS_TREE_METHOD, params);
   }
-});
-
-connection.onInitialized(() => {
-  console.log("language server initialized");
-  if (hasConfigurationCapability) {
-    // Register for all configuration changes.
-    connection.client.register(DidChangeConfigurationNotification.type, undefined).then((e) => {
-      connection.console.log("config change event received.");
-    });
-  }
-  if (hasWorkspaceFolderCapability) {
-    connection.workspace.onDidChangeWorkspaceFolders((_event) => {
-      connection.console.log("Workspace folder change event received.");
-    });
-  }
-});
-
-// The example settings
-interface ExampleSettings {
-  maxNumberOfProblems: number;
-}
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
-
-connection.onDidChangeConfiguration((change) => {
-  globalSettings = <ExampleSettings>(change.settings.languageServerExample || defaultSettings);
 });
 
 // This handler provides the initial list of the completion items.
